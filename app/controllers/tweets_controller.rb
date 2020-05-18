c0ae75796baa22a523792c9c01c328207164061a
Haruka Teduka <th_kiyose@outlook.com>
2020-05-18 13:40:48 +0900

    新規投稿機能を実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,19 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all.order(created_at: :desc)
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(content_params)
+    @tweet.save
+    redirect_to tweets_path
+  end
+
+  private
+  def content_params
+    params.require(:tweet).permit(:content)
+  end
 end
